56c184ecc6a3974a04598ab8caeed135422c8637
Cecilie Bamer <ceciliebamer@gmail.com>
2026-06-09 08:23:33 +0200

    commenting code

--- Failed_perturbations/single_param_pert.py
+++ Failed_perturbations/single_param_pert.py
@@ -1,22 +1,18 @@
 import sympy as sp
-from sympy import pprint , abc , dsolve , Derivative , integrate
-from sympy.abc import a , b , c , g , e , d , f , h , i , j , k , l , m , n , o , p , q , r
-from sympy.solvers.ode.systems import dsolve_system
-from config import rhat0 , betahat0
-from sympy.matrices.expressions import MatMul
 
+"""This code has perturbed the particle in one parameter, epsilon_0, and solves it algebraically"""
 
-#defining the hatted variables
+#Defining the variables
 t = sp.Symbol("t") #time
 dt = sp.Symbol("dt") #time differential
 
 r_0 = sp.Function("r_0")(t) #r0
 r_1 = sp.Function("r_1")(t) #r1
 
-v_r0 = sp.Function("v_r0")(t) #v0
+v_r0 = sp.Function("v_r0")(t) #vr0
 v_r1 = sp.Function("v_r1")(t) #vr1
 
-vrdot_0 = sp.Function("vrdot_0")(t) #vdot0
+vrdot_0 = sp.Function("vrdot_0")(t) #vrdot0
 vrdot_1 = sp.Function("vrdot_1")(t) #vrdot1
 
 theta_0 = sp.Function("theta_0")(t) #theta0
@@ -28,30 +24,28 @@ omega_1 = sp.Function("omega_1")(t) #omega1
 omegadot_0 = sp.Function("omegadot_0")(t) #omegadot0
 omegadot_1 = sp.Function("omegadot_1")(t) #omegadot1
 
-beta_0 = sp.Function("beta0")(t)
-beta_1 = sp.Function("beta1")(t)
+"""Beta and mass relations are derived by hand"""
+beta_0 = sp.Function("beta0")(t) #beta0
+beta_1 = sp.Function("beta1")(t) #beta1
 
-dt_r0 = sp.Symbol("dt_r0")
-dt_r1 = sp.Symbol("dt_r1")
-dtt_r0 = sp.Symbol("dtt_r0")
-dtt_r1 = sp.Symbol("dtt_r1")
+dt_r0 = sp.Symbol("dt_r0") #dr0/dt
+dt_r1 = sp.Symbol("dt_r1") #dr1/dt
+dtt_r0 = sp.Symbol("dtt_r0") #d^2r0/dt^2
+dtt_r1 = sp.Symbol("dtt_r1") #d^2r1/dt^2
 
-dt_theta0 = sp.Symbol("dt_theta0")
-dt_theta1 = sp.Symbol("dt_theta1")
-dtt_theta0 = sp.Symbol("dtt_theta0")
-dtt_theta1 = sp.Symbol("dtt_theta1")
+dt_theta0 = sp.Symbol("dt_theta0") #dtheta0/dt
+dt_theta1 = sp.Symbol("dt_theta1") #dtheta1/dt
+dtt_theta0 = sp.Symbol("dtt_theta0") #d^2theta0/dt^2
+dtt_theta1 = sp.Symbol("dtt_theta1") #d^2theta1/dt^2
 
 B = sp.Symbol("B") #B, initial beta 
-delta = sp.Symbol("delta")
+delta = sp.Symbol("delta") #delta, drag term
 
 epsilon_0 = sp.Symbol("epsilon_0") #epsilon_0
-E_r = sp.Function("E")(r_0) #radial dependent epsilon
-vrdotterm = 6 * epsilon_0 / r_0**4 * r_1 * dt_r0**2 + epsilon_0 / r_0**2 * dtt_r1 - 2 * epsilon_0 / r_0**3 * r_1 * dt_r0
-omegadotterm = - 2 * epsilon_0 / r_0**3 * dt_r0 * dt_theta1 - 6 * epsilon_0 * theta_1 / r_0**4 * dt_r0**2 + epsilon_0 / r_0**2 * dtt_theta1 - 2 * epsilon_0 / r_0**3 * theta_1 * dt_r0 
 
-r_exp = r_0 + epsilon_0 * E_r * r_1 #r perturbed expression
-vr_exp = dt_r0 - 2 * epsilon_0 * r_1 / r_0**3 * dt_r0 + epsilon_0 / r_0**2 * dt_r1 #v perturbed expression
-vrdot_exp = dtt_r0 * (1 - 2 * epsilon_0 * r_1 / r_0**3) - 2 * epsilon_0 / r_0**3 * dt_r0 * dt_r1 + vrdotterm  #vrdot perturbed expression
+r_exp = r_0 + epsilon_0 * r_1 #r perturbed expression
+vr_exp = dt_r0 + epsilon_0 * dt_r1  #v perturbed expression
+vrdot_exp = dtt_r0 + epsilon_0 * dtt_r1 + epsilon_0 * ?????  #vrdot perturbed expression
 
 theta_exp = theta_0 + epsilon_0 * E_r * theta_1 #theta perturbed expression
 omega_exp = dt_theta0 - 2 * epsilon_0 * theta_1 / r_0**3 * dt_r0 + epsilon_0 / r_0**2 * dt_theta1  #omega perturbed expression
